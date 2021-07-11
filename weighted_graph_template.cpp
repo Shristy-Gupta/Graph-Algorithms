9405b1b0b29344b788d825c0cc9c2a99697881cc
Shristy Gupta <shristysgupta@gmail.com>
2021-07-11 11:40:47 +0530

    Create weighted_graph_template.cpp

---
+++ weighted_graph_template.cpp
@@ -0,0 +1,18 @@
+// Weighted Graph in c++
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n,m;
+    cin>>n>>m;
+    //Incase of weight vector<pait<int,int>> adj[n+1]
+    vector<pait<int,int>> adj[n+1];
+    for(int i=0;i<m;i++){
+        int u,v,wt;
+        cin>>u>>v>>wt;
+        adj[u].push_back({v,wt});
+        adj[v].push_back({u,wt});
+    }
+    return 0;
+}
